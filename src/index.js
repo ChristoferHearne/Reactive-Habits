5fae9d72314d13af854f1e42cd77fe9172d21463
Christofer Hearne <chhe1903@student.miun.se>
2021-06-30 14:16:03 +0200

    First rendering of new elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class ToDoList extends React.Component{
+  render(){
+    return(
+      <div className="shopping-list">
+        <h1> Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>WhatsApp</li>
+          <li>Facebook</li>
+          <li>Instagram</li>
+        </ul>
+      </div>
+    );
+  }
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToDoList name="HabitCruncher"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
